638f2f710772f35ffb73c77c1b80d3feea4f7be8
damian <damian.szalimow@onet.eu>
2021-09-05 23:55:17 +0200

    an  destructuring array example

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+{
 // destructuring of array
 
-const dataArr = ['John', 'Rambo', 69, "New York"];
+ dataArr = ['John', 'Rambo', 69, "New York"];
 
 let [firstName, surname, age, city,Non] = dataArr;
 
@@ -11,3 +12,21 @@ city = "Chicago";
 // checking 
 
 console.log(`My name is ${firstName} ${surname}. I am ${age} old and live in ${city}.`);
+}
+
+
+// Usage example
+
+{
+function dA(none){
+    const dataArr = ['John', 'Rambo', 69, "New York", none];
+
+    return dataArr
+}
+
+let [firstName, surname, age, city,country] = dA("USA");
+
+city = "Chicago"; 
+
+console.log(`We can use variables by name: firstname is ${firstName}, surname is ${surname}, age is ${age}, city is ${city} and country is ${country}.Normally we have to type a array index like 1,2,3,4,5.`);
+};
