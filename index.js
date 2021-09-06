c8f4af21ff6f838f4722a98a490126b3db053fed
damian <damian.szalimow@onet.eu>
2021-09-06 21:54:22 +0200

    add commonJS module usage

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const { print } = require("./m1");
-print("Ala");
+//const fn = require("./m1");
+//const {example} = require("./m1");
+//const {a : { FirstName }} = fn;
+//console.log(fn.b.FirstName);
+//console.log(b)
+
+//let list = { FirstName: 'John', SecondName: 'Rambo', Age: 23 };
+//const {FirstName} = list;
+
+//console.log(fn)
+
+// my previous failed tries ;)
+
+const {a: { b }} = require('./m1');
+
+console.log(b)
