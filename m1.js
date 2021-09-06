279496877317df4f951ccc6293cad1c3702ec9ac
damian <damian.szalimow@onet.eu>
2021-09-06 22:19:00 +0200

    add m1.js

---
+++ m1.js
@@ -0,0 +1,15 @@
+
+module.exports = { a: {b: 'c'}};
+
+//function a(txt) {
+//    console.log(`${txt}`);
+//}
+//module.exports.a = a; // module.exports is an object 
+
+//module.exports.example = { 
+//    FirstName : 'John',
+//    SecondName: 'Rambo',
+//    Age: 23,
+//};
+
+// my previous failed tries ;)
