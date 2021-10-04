6ac71ad1c20a4bcbc669a43ded9d0c5397394b51
damian <damian.szalimow@onet.eu>
2021-10-04 23:35:19 +0200

    add layout changes

--- index.js
+++ index.js
@@ -62,3 +62,20 @@ console.log(`We can use variables by name: firstname is ${firstName}, surname is
 
     
 }
+/* ================================================================================================
+/* toggle 
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+}) */ 
